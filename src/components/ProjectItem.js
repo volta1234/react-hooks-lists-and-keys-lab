0f1688d1ab3fb9cc3dce2a55a253285397b56ff8
CALVINCE <calvince.ogwel@gmail.com>
2023-01-12 20:28:34 +0300

    completed compulsory lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,9 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        <span>{technologies[0]}</span>
+        <span>{technologies[1]}</span>
+        <span>{technologies[2]}</span>
       </div>
     </div>
   );
